65da40de467d08c8a0882947fadb1f1d3bda88dd
Dhritiman <dhritimanbhardwaj@gmail.com>
2022-09-28 12:14:29 +0530

    Migrations

--- backend/attendance/models.py
+++ backend/attendance/models.py
@@ -3,10 +3,17 @@ from school.models import Teacher, Student
 
 # Create your models here.
 
+
 class Attendance(models.Model):
-    date = models.DateTimeField(auto_now_add=True)
-    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='student')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='teacher')
+    date = models.DateField(auto_now_add=True)
+    student = models.ForeignKey(
+        Student, on_delete=models.CASCADE, related_name='student')
+    teacher = models.ForeignKey(
+        Teacher, on_delete=models.CASCADE, related_name='teacher')
     attendance = models.BooleanField(default=False)
+
     class Meta:
-        unique_together = ('date', 'student')  
+        constraints = [
+            models.UniqueConstraint(
+                fields=['date', 'student'],  name="unique_attendance")
+        ]
--- backend/school/models.py
+++ backend/school/models.py
@@ -4,38 +4,50 @@ from authentication.models import User
 
 # Create your models here.
 
-def create_path_images(self,filename):
-    email = self.parent_email.split('@')[0]
-    return f'image/{email}/{filename}'
-
-class Teacher(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
-    contact_no = models.CharField(max_length=10,unique=True)
-
-    def __str__(self):
-        return self.name
 
 class Section(models.Model):
     name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
-    
+
+
 class Class(models.Model):
     name = models.CharField(max_length=50)
-    section = models.ForeignKey(to=Section,on_delete=models.CASCADE,related_name='section')
+    section = models.ForeignKey(
+        to=Section, on_delete=models.CASCADE, related_name='section')
+
+    def __str__(self):
+        return self.name + " " + self.section.name
+
+
+def create_path_images(self, filename):
+    email = self.email.split('@')[0]
+    return f'image/{email}/{filename}'
+
+
+class Teacher(models.Model):
+    id = models.CharField(primary_key=True, max_length=20)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    contact = models.CharField(max_length=10, unique=True)
+    _class = models.ForeignKey(
+        to=Class, on_delete=models.CASCADE, related_name='teacher_class')
+    designation = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
-    
+
+
 class Student(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.CharField(primary_key=True, max_length=20)
     name = models.CharField(max_length=50)
-    parent_email = models.EmailField()
-    parent_no = models.CharField(max_length=10, unique=True)
-    student_class = models.ForeignKey(to=Class, on_delete=models.CASCADE,related_name='student_class')
+    email = models.EmailField()
+    contact = models.CharField(max_length=10, unique=True)
+    _class = models.ForeignKey(
+        to=Class, on_delete=models.CASCADE, related_name='student_class')
     photo = models.ImageField(upload_to=create_path_images)
+    admission_date = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.name
