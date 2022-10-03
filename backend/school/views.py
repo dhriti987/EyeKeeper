a025718d58ae0db4cc611c752bfb71adf0bdd6e8
Dhritiman <dhritimanbhardwaj@gmail.com>
2022-10-03 22:39:19 +0530

    minor fixes

--- backend/school/views.py
+++ backend/school/views.py
@@ -1,9 +1,11 @@
+from flask import Response
 from rest_framework import generics
 from school.models import Class, Student, Teacher
 
 from school.serializers import ClassSerializer, StudentSerializer, TeacherSerializer
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
@@ -26,3 +28,12 @@ class TeacherAPIView(generics.ListCreateAPIView):
 
     serializer_class = TeacherSerializer
     queryset = Teacher.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        try:
+            new_user = get_user_model().objects.create_user(
+                email=request.data['email'], password="abcdefgh")
+            request.data['user'] = new_user.pk
+        except:
+            return Response("User with This Email Already Exist", status=404)
+        return super().post(request, *args, **kwargs)
